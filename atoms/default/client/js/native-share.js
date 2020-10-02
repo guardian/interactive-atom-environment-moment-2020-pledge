945eb15060fcc7ac15df1289e5639f64ce59bf4a
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-10-02 10:07:07 +0200

    updates native share thing

--- atoms/default/client/js/native-share.js
+++ atoms/default/client/js/native-share.js
@@ -3,8 +3,8 @@ setupNativeShare();
 
 function setupNativeShare() {
   const shareData = {
-    text: 'The Guardian’s environment pledge',
-    url: 'https://www.theguardian.com',
+    text: 'The Guardian’s climate pledge',
+    url: 'https://www.theguardian.com/environment/ng-interactive/2020/oct/05/the-guardian-climate-pledge-2020-environment-emergency-carbon-emissions',
   }
   const shareEl = document.querySelector('.support-and-share__share__sheet');
 
