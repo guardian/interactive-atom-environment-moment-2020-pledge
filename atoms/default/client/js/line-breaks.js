6c6e62f509b8563f5bcc5c64877556a1473aaaa7
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-10-09 15:48:47 +0200

    remove line break logs

--- atoms/default/client/js/line-breaks.js
+++ atoms/default/client/js/line-breaks.js
@@ -1,7 +1,5 @@
 let pointsAll = document.querySelectorAll('ul li strong~br');
-console.log(pointsAll.length)
 pointsAll.forEach((brEl) => {
-  console.log('point', brEl);
   let fakeBrEl = document.createElement('span');
   fakeBrEl.classList.add('br');
   brEl.parentElement.insertBefore(fakeBrEl, brEl)
