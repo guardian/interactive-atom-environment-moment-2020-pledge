73b72a9aa6e40d1d07dcd2fe0ece55603c27e8ab
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-10-09 15:43:08 +0200

    pledge line breaks js

--- atoms/default/client/js/main.js
+++ atoms/default/client/js/main.js
@@ -1,5 +1,5 @@
 // console.log('1131');
-['structure-dom', 'copy-social-buttons', 'read-more', 'sticky-header', 'native-share'].forEach((n) => {
+['structure-dom', 'copy-social-buttons', 'read-more', 'sticky-header', 'native-share', 'line-breaks'].forEach((n) => {
   var el = document.createElement('script');
   el.src = '<%= atomPath %>/' + n + '.js';
   document.body.appendChild(el);
